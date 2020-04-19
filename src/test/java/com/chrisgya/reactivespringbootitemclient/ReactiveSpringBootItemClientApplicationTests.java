074fa293a5a7a01658ab14b4445758370173107d
christian.gyaban <christian.gyaban@interswitchgroup.com>
2020-04-19 13:36:00 +0100

    first commit

---
+++ src/test/java/com/chrisgya/reactivespringbootitemclient/ReactiveSpringBootItemClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisgya.reactivespringbootitemclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringBootItemClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
